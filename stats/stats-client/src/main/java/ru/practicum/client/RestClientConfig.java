a983ef280c0a4097571422364c195d944dc4d3d9
StanislavVE77 <s.emelyanov@complex-solutions.ru>
2025-05-26 00:30:19 +0300

    Исправление ошибок 5

--- stats/stats-client/src/main/java/ru/practicum/client/RestClientConfig.java
+++ stats/stats-client/src/main/java/ru/practicum/client/RestClientConfig.java
@@ -1,6 +1,5 @@
 package ru.practicum.client;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
