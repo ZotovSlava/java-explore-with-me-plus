c7444888ad863c093b11e078bb90fc54703ccee9
StanislavVE77 <s.emelyanov@complex-solutions.ru>
2025-05-26 12:31:57 +0300

    Изменение настроек

--- stats/stats-client/src/main/java/ru/practicum/client/StatRestClient.java
+++ stats/stats-client/src/main/java/ru/practicum/client/StatRestClient.java
@@ -35,7 +35,7 @@ public class StatRestClient {
 
 
     public List<StatsDto> getStats(String start, String end, List<String> uris, boolean unique) {
-        String url = UriComponentsBuilder.fromHttpUrl("http://localhost:9090/stats")
+        String url = UriComponentsBuilder.fromHttpUrl("/stats")
                 .queryParam("start", start)
                 .queryParam("end", end)
                 .queryParam("uris", uris != null && !uris.isEmpty() ? String.join(",", uris) : null)
